91b68d4a6c7c5261472e83693bc310d73ce075da
Micael Persson <micael.c.persson@gmail.com>
2014-09-30 15:35:15 +0200

    Created class SecretNumbers.

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -8,5 +8,16 @@ namespace _1DV402.S2.L1A
 {
     class SecretNumber
     {
+        private int _count;
+        private int _number;
+        const int MaxNumberOfGuesses = 7;
+
+        public void Initialize()
+        {
+            Random randomNumber = new Random();
+            _number = randomNumber.Next(1, 101);
+            _count = 0;
+        }
+
     }
 }
