9a2d59471d82d577e5794c10cc58b449c7b1a362
Micael Persson <micael.c.persson@gmail.com>
2014-09-30 17:03:27 +0200

    Working on the MakeGuess-method.

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -10,13 +10,41 @@ namespace _1DV402.S2.L1A
     {
         private int _count;
         private int _number;
-        const int MaxNumberOfGuesses = 7;
+        public const int MaxNumberOfGuesses = 7;
 
         public void Initialize()
         {
             Random randomNumber = new Random();
             _number = randomNumber.Next(1, 101);
             _count = 0;
+        }
+
+        public SecretNumber()
+        {
+            Initialize();
+        }
+
+        public bool MakeGuess(int number)
+        {
+            
+                if (number < _number)
+                {
+                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                    _count = _count + 1;
+                    return false;
+                } 
+                else if (number > _number)
+	            {
+                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                    _count = _count + 1;
+                    return false;
+	            }
+                else
+                {
+                    Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} försök", _count);
+                    return true;
+                }
+            
         }
 
     }
