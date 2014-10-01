8d8fe0eb44e8c02f2bdab84643ec27ad4422df15
Micael Persson <micael.c.persson@gmail.com>
2014-10-01 11:32:59 +0200

    Cleaned up the code and added some comments.

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace _1DV402.S2.L1A
 {
-    class SecretNumber
+    public class SecretNumber
     {
+        // Declaring variables and a constant
         private int _count;
         private int _number;
         public const int MaxNumberOfGuesses = 7;
 
+        // Method used to assign a random number to _number and reseting _count to 0
         public void Initialize()
         {
             Random randomNumber = new Random();
@@ -19,6 +21,7 @@ namespace _1DV402.S2.L1A
             _count = 0;
         }
 
+        // Constructor
         public SecretNumber()
         {
             Initialize();
@@ -26,39 +29,42 @@ namespace _1DV402.S2.L1A
 
         public bool MakeGuess(int number)
         {
-            if (_count < MaxNumberOfGuesses)
-            {
-                if (number < 1)
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-
-                if (number > 100)
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-                
-                if (number < _number)
-                {
-                    _count = _count + 1;
-                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));                    
-                    return false;
-                }
-                else if (number > _number)
-                {
-                    _count = _count + 1;
-                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));                    
-                    return false;
-                }
-                else
-                {
-                    Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} försök", _count);
-                    return true;
-                }
-            }
-            else
+            // Checking if MaxNumberOfGuesses limit has been exceeded and if the number is between 1 and 100
+            if (_count >= MaxNumberOfGuesses)
             {                
                 throw new ApplicationException();
+            }
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            // Checking if the number is low/high/correct and printing the relevant message 
+            if (number < _number)
+            {
+                _count = _count + 1;
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                if (_count == MaxNumberOfGuesses)
+                {
+                    Console.WriteLine("Det hemliga talet är:", _number);
+                }    
+                return false;
+            }
+            else if (number > _number)
+            {
+                _count = _count + 1;
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                if (_count == MaxNumberOfGuesses)
+                {
+                    Console.WriteLine("Det hemliga talet är: {0}", _number);
+                }  
+                return false;
+            }
+            else
+            {
+                _count = _count + 1;
+                Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} försök", _count);
+                return true;
             }
         }
     }
